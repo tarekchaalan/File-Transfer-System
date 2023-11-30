94985b0e4ade6ee816f7f63b594963d65acb30d3
Tarek Chaalan <tchaalan23@icloud.com>
2023-11-29 20:24:18 -0800

    Added Port # in Command Line Functionality

---
+++ client.py
@@ -0,0 +1,126 @@
+import socket
+import os
+import sys
+
+def find_file(name, path='.'):
+    """
+    Search for a file with a given name within a directory and its subdirectories.
+    """
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            return os.path.join(root, name)
+    return None
+
+def send_command(connSock, command):
+    """
+    Send a command to the server via a socket, padding it to a fixed length.
+    """
+    connSock.sendall(command.encode().ljust(100, b'\0'))
+
+def receive_response(connSock):
+    """
+    Receive a response from the server via a socket.
+    """
+    response = connSock.recv(1024).decode().strip('\0')
+    return response
+
+def send_file(dataSock, fileName):
+    """
+    Send a file to the server over a socket.
+    """
+    if not os.path.isfile(fileName):
+        print(f"File {fileName} does not exist.")
+        return False
+
+    fileSize = os.path.getsize(fileName)
+    dataSock.sendall(str(fileSize).encode().ljust(10, b'\0'))
+
+    with open(fileName, "rb") as fileObj:
+        while True:
+            fileData = fileObj.read(65536)
+            if not fileData:
+                break
+            dataSock.sendall(fileData)
+
+    print(f"Sent {fileName} of {fileSize} bytes to the server.")
+    return True
+
+def receive_file(dataSock, fileName, clientPort):
+    """
+    Receive a file from the server and save it to a client-specific directory.
+    """
+    clientDir = f"Client_{clientPort}_Received"
+    if not os.path.exists(clientDir):
+        os.makedirs(clientDir)
+
+    filePath = os.path.join(clientDir, fileName)
+
+    with open(filePath, "wb") as fileObj:
+        while True:
+            fileData = dataSock.recv(65536)
+            if not fileData:
+                break
+            fileObj.write(fileData)
+
+    print(f"Received {fileName} and saved in '{clientDir}'.")
+
+def main():
+    """
+    Main function to run the client. Handles connection to the server and user inputs.
+    """
+    serverAddr = "localhost"
+    if len(sys.argv) > 1:
+        serverPort = int(sys.argv[1])
+        print("Connecting to server port #" + str(serverPort))
+    else:
+        print("Error: Please provide an open port number")
+        sys.exit(1)
+
+    try:
+        controlSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        controlSock.connect((serverAddr, serverPort))
+        localPort = controlSock.getsockname()[1]  # Client's port number
+    except socket.error as e:
+        print(f"Socket error: {e}")
+        return
+    except Exception as e:
+        print(f"Other exception: {e}")
+        return
+
+    while True:
+        command = input("\nEnter command (put/get/ls/quit): ").strip()
+
+        send_command(controlSock, command)
+
+        if command == 'quit':
+            break
+        elif command.startswith('put'):
+            _, fileName = command.split()
+            found = find_file(fileName)
+            if found:
+                fileName = found
+            dataSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            dataSock.connect((serverAddr, serverPort + 1))
+            send_file(dataSock, fileName)
+            dataSock.close()
+        elif command.startswith('get'):
+            _, fileName = command.split()
+            response = receive_response(controlSock)
+            if "Sending" in response:
+                dataSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                dataSock.connect((serverAddr, serverPort + 1))
+                receive_file(dataSock, fileName, localPort)
+                dataSock.close()
+            else:
+                print(response)
+        elif command == 'ls':
+            response = receive_response(controlSock)
+            print("Files on server:", response)
+
+        if not command.startswith(('get', 'ls')):
+            response = receive_response(controlSock)
+
+    controlSock.close()
+
+if __name__ == "__main__":
+    main()
---
+++ server.py
@@ -0,0 +1,149 @@
+import socket
+import threading
+import os
+import sys
+
+def recv_all(sock, numBytes):
+    """
+    Receive a specific number of bytes from a socket.
+    """
+    recvBuff = b""
+    while len(recvBuff) < numBytes:
+        tmpBuff = sock.recv(numBytes)
+        if not tmpBuff:
+            break
+        recvBuff += tmpBuff
+    return recvBuff
+
+def send_file(dataSock, filePath):
+    """
+    Send a file over a socket.
+    """
+    with open(filePath, "rb") as fileObj:
+        while True:
+            fileData = fileObj.read(65536)
+            if not fileData:
+                break
+            dataSock.sendall(fileData)
+
+def handle_client(controlSock, addr):
+    """
+    Handle commands from a connected client.
+    """
+    clientIP, clientPort = addr
+    try:
+        while True:
+            commandBuff = recv_all(controlSock, 100)
+            command = commandBuff.decode().strip('\0')
+            if not command:
+                break
+
+            if command == 'quit':
+                controlSock.sendall("Quitting".encode().ljust(100, b'\0'))
+                break
+            elif command.startswith('put'):
+                _, fileName = command.split()
+                baseFileName = os.path.basename(fileName)
+
+                if not os.path.exists("Server_Files"):
+                    os.makedirs("Server_Files")
+
+                dataSock, _ = welcomeDataSock.accept()
+                fileSizeBuff = recv_all(dataSock, 10)
+                fileSizeStr = fileSizeBuff.decode().strip('\0').strip()
+                if fileSizeStr.isdigit():
+                    fileSize = int(fileSizeStr)
+                else:
+                    print("Invalid file size received.")
+                    dataSock.close()
+                    continue
+
+                receivedSize = 0
+                with open(f"Server_Files/{baseFileName}", "wb") as f:
+                    while receivedSize < fileSize:
+                        fileData = dataSock.recv(min(65536, fileSize - receivedSize))
+                        if not fileData:
+                            break
+                        f.write(fileData)
+                        receivedSize += len(fileData)
+
+                dataSock.close()
+                controlSock.sendall("File received".encode().ljust(100, b'\0'))
+                print(f"Received {baseFileName} from Client {clientPort}")
+            elif command.startswith('get'):
+                _, fileName = command.split()
+                filePath = os.path.join("Server_Files", fileName)
+
+                if os.path.isfile(filePath):
+                    controlSock.sendall("File found. Sending...".encode().ljust(100, b'\0'))
+                    print(f"Sending {fileName} to Client {clientPort}")
+
+                    dataSock, _ = welcomeDataSock.accept()
+                    send_file(dataSock, filePath)
+                    dataSock.close()
+                else:
+                    controlSock.sendall("File not found".encode().ljust(100, b'\0'))
+            elif command == 'ls':
+                if not os.path.exists("Server_Files"):
+                    os.makedirs("Server_Files")
+
+                files = os.listdir("Server_Files")
+                if files:
+                    fileList = ', '.join(files)
+                else:
+                    fileList = 'NONE'
+
+                controlSock.sendall(fileList.encode().ljust(100, b'\0'))
+                print(f"Showing Client {clientPort} the server files")
+
+    except Exception as e:
+        print(f"\nError: {e}")
+    finally:
+        controlSock.close()
+        print(f"\nConnection closed with {addr}.\n")
+
+def main():
+    """
+    Main function to start the server and accept client connections.
+    """
+    global welcomeDataSock
+    if len(sys.argv) > 1:
+        listenPort = int(sys.argv[1])
+        print("Opening port #" + str(listenPort))
+    else:
+        print("Error: Please provide an open port number")
+        sys.exit(1)
+    dataPort = listenPort + 1
+
+    try:
+        welcomeSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        welcomeSock.bind(('', listenPort))
+        welcomeSock.listen(5)
+
+        welcomeDataSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        welcomeDataSock.bind(('', dataPort))
+        welcomeDataSock.listen(5)
+
+        print("\nServer started. Waiting for connections...")
+
+        while True:
+            try:
+                controlSock, addr = welcomeSock.accept()
+                print(f"Connected to Client: {addr}\n")
+                clientThread = threading.Thread(target=handle_client, args=(controlSock, addr))
+                clientThread.start()
+            except socket.timeout:
+                pass
+
+    except KeyboardInterrupt:
+        print("\nServer is shutting down...")
+    except socket.error as e:
+        print(f"Socket error: {e}")
+    except Exception as e:
+        print(f"Other exception: {e}")
+    finally:
+        welcomeSock.close()
+        welcomeDataSock.close()
+
+if __name__ == "__main__":
+    main()
